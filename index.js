c918d5dccbc1f5d63595a16d4268dd5e0cd7178b
Octávio Richter <4429577+OctavioBR@users.noreply.github.com>
2024-11-17 17:48:37 -0300

    Staffan Olsson code

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express')
+const morgan = require('morgan');
 var app = express()
+const os = require('os');
+const jwt = require('jsonwebtoken');
+var concat = require('concat-stream');
+
+app.set('json spaces', 2);
+
+app.use(morgan('combined'));
+
+app.use(function(req, res, next){
+  req.pipe(concat(function(data){
+    req.body = data.toString('utf8');
+    next();
+  }));
+});
 
 app.all('*', (req, res) => {
-  res.json({
+  const echo = {
     path: req.path,
     headers: req.headers,
     method: req.method,
@@ -16,7 +31,38 @@ app.all('*', (req, res) => {
     query: req.query,
     subdomains: req.subdomains,
     xhr: req.xhr,
-  })
-})
+    os: {
+      hostname: os.hostname()
+    }
+  };
+  if (process.env.JWT_HEADER) {
+    const token = req.headers[process.env.JWT_HEADER.toLowerCase()];
+    if (!token) {
+      echo.jwt = token;
+    } else {
+      const decoded = jwt.decode(token, {complete: true});
+      echo.jwt = decoded;
+    }
+  }
+  res.json(echo);
+});
 
-app.listen(process.env.PORT || 3000)
+const server = app.listen(process.env.PORT || 80);
+let calledClose = false;
+
+process.on('exit', function () {
+  if (calledClose) return;
+  console.log('Got exit event. Trying to stop Express server.');
+  server.close(function() {
+    console.log("Express server closed");
+  });
+});
+
+process.on('SIGINT', function() {
+  console.log('Got SIGINT. Trying to exit gracefully.');
+  calledClose = true;
+  server.close(function() {
+    console.log("Exoress server closed. Asking process to exit.");
+    process.exit()
+  });
+});
